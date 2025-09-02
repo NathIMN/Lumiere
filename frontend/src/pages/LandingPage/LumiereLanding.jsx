ec2968e4f735fb01831285510d844fe226144f33
Nadeena-Srioshi <nadshrio@gmail.com>
2025-09-02 22:41:31 +0530

    routes

--- frontend/src/pages/LandingPage/LumiereLanding.jsx
+++ frontend/src/pages/LandingPage/LumiereLanding.jsx
@@ -1,15 +1,15 @@
 import { Particles } from '../../components/landing/Particles';
 import { HeaderLanding } from '../../components/landing/HeaderLanding';
 import { CardsFeatures } from '../../components/landing/CardsFeatures';
-import { HorizontalCarousel } from '../../components/landing/HorizontalCarousel'; 
+import { HorizontalCarousel } from '../../components/landing/HorizontalCarousel';
 import { HeroLanding } from '../../components/landing/HeroLanding';
-
+import ScrollStack, { ScrollStackItem } from '../../components/landing/ScrollStack';
 
 export const LumiereLanding = () => {
   return (
     <div className="bg-white relative w-full">
 
-      <HeaderLanding/>
+      <HeaderLanding />
 
       <Particles
         particleColors={['#ff8372', '#e092b5', '#0ac8e8']}
@@ -23,12 +23,25 @@ export const LumiereLanding = () => {
         className="absolute inset-0 z-0 h-[800px]"
       />
 
-      <HeroLanding/>
+      <HeroLanding />
 
-      <HorizontalCarousel/>
+      <ScrollStack>
+        <ScrollStackItem>
+          <h2>Card 1</h2>
+          <p>This is the first card in the stack</p>
+        </ScrollStackItem>
+        <ScrollStackItem>
+          <h2>Card 2</h2>
+          <p>This is the second card in the stack</p>
+        </ScrollStackItem>
+        <ScrollStackItem>
+          <h2>Card 3</h2>
+          <p>This is the third card in the stack</p>
+        </ScrollStackItem>
+      </ScrollStack>
 
-      <CardsFeatures/>
-      
+      <CardsFeatures />
+
     </div>
   )
 }
