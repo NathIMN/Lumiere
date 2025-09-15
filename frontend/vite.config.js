3c69b479f580b380009db10f281daf40ff04062d
Nadeena-Srioshi <nadshrio@gmail.com>
2025-09-15 16:47:32 +0530

    fixes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    host: '0.0.0.0', // Listen on all network interfaces
+    //host: '0.0.0.0', // Listen on all network interfaces
     port: 5173,      // Explicit port
   }
 })
